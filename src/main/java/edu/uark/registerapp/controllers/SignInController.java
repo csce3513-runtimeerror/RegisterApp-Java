eed31b27f35b9d03cb0fe4d7d3e231449c896be4
sicilyg <sicilyguo321@gmail.com>
2020-02-27 13:48:17 -0600

    Java class created

---
+++ src/main/java/edu/uark/registerapp/controllers/SignInController.java
@@ -0,0 +1,17 @@
+package edu.uark.registerapp.controllers;
+
+import java.util.UUID;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.RestController;
+
+import edu.uark.registerapp.commands.products.ProductCreateCommand;
+import edu.uark.registerapp.commands.products.ProductDeleteCommand;
+import edu.uark.registerapp.commands.products.ProductUpdateCommand;
+import edu.uark.registerapp.models.api.ApiResponse;
+import edu.uark.registerapp.models.api.Product;
---
+++ src/main/java/edu/uark/registerapp/controllers/SignInRouteController.java
@@ -0,0 +1,16 @@
+package edu.uark.registerapp.controllers;
+
+import java.util.UUID;
+
+import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import edu.uark.registerapp.commands.products.ProductQuery;
+import edu.uark.registerapp.controllers.enums.ViewModelNames;
+import edu.uark.registerapp.controllers.enums.ViewNames;
+import edu.uark.registerapp.models.api.Product;
