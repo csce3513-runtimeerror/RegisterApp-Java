0aca49436de1cbb36699eb096c6ab6cc7b93b192
sicilyg <sicilyguo321@gmail.com>
2020-02-27 19:26:54 -0600

    Update on 2.4

--- src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
+++ src/main/java/edu/uark/registerapp/models/api/EmployeeSignIn.java
@@ -1,6 +1,19 @@
 package edu.uark.registerapp.models.api; 
 
 public class EmployeeSignIn {
-    private String EmployeeId;
+    private String employeeId;
     private String password;
+
+    public EmployeeSignIn(String employeeId, String password){
+        this.employeeId = employeeId;
+        this.password = password;
+    }
+
+    public String getEmployeeId(){
+        return employeeId;
+    }
+    public String getPassword() {
+        return password;
+    }
+    
 }
