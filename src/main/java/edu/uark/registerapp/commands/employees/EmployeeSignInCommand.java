3f9c542e28a79f18e359b02418a1c943c122a438
sicilyg <sicilyguo321@gmail.com>
2020-03-04 14:03:09 -0600

    verifying password functions

--- src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
+++ src/main/java/edu/uark/registerapp/commands/employees/EmployeeSignInCommand.java
@@ -2,9 +2,12 @@ package edu.uark.registerapp.commands.employees;
 
 import java.util.Optional;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 
 import edu.uark.registerapp.commands.VoidCommandInterface;
+import edu.uark.registerapp.commands.employees.helpers.EmployeeHelper;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.models.api.EmployeeSignIn;
 import edu.uark.registerapp.models.entities.EmployeeEntity;
@@ -15,18 +18,28 @@ public class EmployeeSignInCommand implements VoidCommandInterface {
     private EmployeeSignIn signIn = new EmployeeSignIn();
     private String sessionKey;
 
+    @Transactional
     @Override
     public void execute() {
         final Optional<EmployeeEntity> employeeEntity = this.employeeRepository.
             findByEmployeeId(Integer.parseInt(signIn.getEmployeeId()));
         if(employeeEntity.isPresent()){
             //verify the password
-            
+            final byte[] correctP = employeeEntity.get().getPassword();
+            byte[] currentP = EmployeeHelper.hashPassword(signIn.getPassword());
+            if(correctP.equals(currentP)){
+                
+            }
+            else {
+
+            }
         }
         else{
             throw new NotFoundException("Employee");
         }
     }
+
+
     @Autowired
 	private EmployeeRepository employeeRepository;
 }
