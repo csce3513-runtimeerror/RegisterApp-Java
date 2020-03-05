89cbe855aaf1b88b7ccd45a7007a1b811f6c3a3a
raculber <rculbert5678@gmail>
2020-03-05 14:07:33 -0600

    Changed emp rest controller

--- src/main/java/edu/uark/registerapp/controllers/EmployeeRestController.java
+++ src/main/java/edu/uark/registerapp/controllers/EmployeeRestController.java
@@ -6,6 +6,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
 import org.apache.commons.lang3.StringUtils;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import edu.uark.registerapp.commands.employees.ActiveEmployeeExistsQuery;
 import edu.uark.registerapp.commands.exceptions.NotFoundException;
 import edu.uark.registerapp.controllers.enums.QueryParameterNames;
 import edu.uark.registerapp.controllers.enums.ViewNames;
@@ -24,19 +26,17 @@ import edu.uark.registerapp.models.entities.EmployeeEntity;
 @RequestMapping(value = "/api/employee")
 public class EmployeeRestController extends BaseRestController {
 	@RequestMapping(value = "/", method = RequestMethod.POST)
-	public @ResponseBody ApiResponse createEmployee(
-		@RequestBody final Employee employee,
-		final HttpServletRequest request,
-		final HttpServletResponse response
-	) {
+	public @ResponseBody ApiResponse createEmployee(@RequestBody final Employee employee,
+			final HttpServletRequest request, final HttpServletResponse response) {
 
 		boolean isInitialEmployee = false;
 		ApiResponse canCreateEmployeeResponse;
 
 		try {
 			// TODO: Query if any active employees exist
-			canCreateEmployeeResponse =
-				this.redirectUserNotElevated(request, response);
+			active.execute();
+
+			canCreateEmployeeResponse = this.redirectUserNotElevated(request, response);
 		} catch (final NotFoundException e) {
 			isInitialEmployee = true;
 			canCreateEmployeeResponse = new ApiResponse();
@@ -50,29 +50,20 @@ public class EmployeeRestController extends BaseRestController {
 		final Employee createdEmployee = new Employee();
 		EmployeeEntity emp = new EmployeeEntity(employee);
 
-
 		if (isInitialEmployee) {
-			createdEmployee
-				.setRedirectUrl(
-					ViewNames.SIGN_IN.getRoute().concat(
-						this.buildInitialQueryParameter(
-							QueryParameterNames.EMPLOYEE_ID.getValue(),
-							createdEmployee.getEmployeeId())));
+			createdEmployee.setRedirectUrl(ViewNames.SIGN_IN.getRoute().concat(this.buildInitialQueryParameter(
+					QueryParameterNames.EMPLOYEE_ID.getValue(), createdEmployee.getEmployeeId())));
 		}
 
 		return createdEmployee.setIsInitialEmployee(isInitialEmployee);
 	}
 
 	@RequestMapping(value = "/{employeeId}", method = RequestMethod.PATCH)
-	public @ResponseBody ApiResponse updateEmployee(
-		@PathVariable final UUID employeeId,
-		@RequestBody final Employee employee,
-		final HttpServletRequest request,
-		final HttpServletResponse response
-	) {
+	public @ResponseBody ApiResponse updateEmployee(@PathVariable final UUID employeeId,
+			@RequestBody final Employee employee, final HttpServletRequest request,
+			final HttpServletResponse response) {
 
-		final ApiResponse elevatedUserResponse =
-			this.redirectUserNotElevated(request, response);
+		final ApiResponse elevatedUserResponse = this.redirectUserNotElevated(request, response);
 		if (!elevatedUserResponse.getRedirectUrl().equals(StringUtils.EMPTY)) {
 			return elevatedUserResponse;
 		}
@@ -81,7 +72,6 @@ public class EmployeeRestController extends BaseRestController {
 		return employee;
 	}
 
-	private ApiResponse redirectUserNotElevated(HttpServletRequest request, HttpServletResponse response) {
-		return null;
-	}
+	@Autowired
+	private ActiveEmployeeExistsQuery active;
 }
