52468265d1e061e618255b646f0e01c268275646
raculber <rculbert5678@gmail>
2020-03-05 16:18:24 -0600

    jkahdj

--- src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
+++ src/main/java/edu/uark/registerapp/controllers/EmployeeDetailRouteController.java
@@ -52,7 +52,8 @@ public class EmployeeDetailRouteController extends BaseRouteController {
 		// check if employees exist
 		// If no employees exist Should immediately redirect[4] to the Employee Detail
 		// view/document
-		if (!employeeRepository.existsByEmployeeId(Integer.parseInt(employeeId)) || employeeClassification.isElevatedUser(employeeClassification.getClassification())) {
+		if (!employeeRepository.existsByEmployeeId(Integer.parseInt(employeeId)) || 
+		employeeClassification.isElevatedUser(employeeClassification.getClassification())) {
 			final ModelAndView mv = new ModelAndView(ViewNames.EMPLOYEE_DETAIL.getViewName());
 			mv.addObject("employee", new Employee());
 			return mv;
